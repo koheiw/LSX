35d04127edfe4ac1437b95e1136bdeac72b80507
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-05-11 15:48:46 +0100

    Adds an example for a token removal function

--- R/tokens.R
+++ R/tokens.R
@@ -1,3 +1,10 @@
+
+#' @examples
+#' docs <- readLines('/home/kohei/projects/immigration/data/uk_img/2009-2010.txt')
+#' sents <- tokenize(docs, what='sentence', simplify = TRUE)
+#' tokens <- tokenize(sents, removePunct=TRUE, removeNumbers=TRUE)
+#' tokens2 <- removeTokens(tokens, c('are', 'is', 'be'), spacer=TRUE)
+#'
 #' @export
 removeTokens <- function(tokens, types_remove, spacer=FALSE){
   tokens2 <- select_tokens_cppl(tokens, types_remove, TRUE, spacer)
