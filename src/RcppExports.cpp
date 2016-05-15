b46cbd524115475f2414ff1a1b304f97475988bc
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-05-15 15:29:09 +0100

    Streamlned entryword selection

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -19,17 +19,3 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
-// select_tokens_cppl
-Rcpp::List select_tokens_cppl(SEXP x, const std::vector< std::string >& types, const bool& remove, const bool& spacer);
-RcppExport SEXP LSS_select_tokens_cppl(SEXP xSEXP, SEXP typesSEXP, SEXP removeSEXP, SEXP spacerSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< SEXP >::type x(xSEXP);
-    Rcpp::traits::input_parameter< const std::vector< std::string >& >::type types(typesSEXP);
-    Rcpp::traits::input_parameter< const bool& >::type remove(removeSEXP);
-    Rcpp::traits::input_parameter< const bool& >::type spacer(spacerSEXP);
-    __result = Rcpp::wrap(select_tokens_cppl(x, types, remove, spacer));
-    return __result;
-END_RCPP
-}
