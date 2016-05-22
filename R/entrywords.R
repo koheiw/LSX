6159378ec97fa244957617fc0772145041e2be07
Kohei Watanabe <watanabe.kohei@gmail.com>
2016-05-22 23:38:15 +0100

    Small fix

--- R/entrywords.R
+++ R/entrywords.R
@@ -37,8 +37,6 @@ count_collocates <- function(tokens, target, target_negative, window=10){
 
 regex2fixed <- function(regex, types, case_insensitive=TRUE, ...){
   regex_joined <- paste0(regex, collapse = "|")
-  types <- unique(unlist(x, use.names=FALSE))
-  # Get all the unique types that match regex
   types_match <- types[stringi::stri_detect_regex(types, regex_joined, case_insensitive = case_insensitive, ...)]
   return(types_match)
 }
