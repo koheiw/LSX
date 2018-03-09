8cc09135605413b2d232ad60e90fad9d03069ff4
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-03-09 10:12:47 +0000

    Add Russian protest model

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' A fitted LSS model on street protest in Russia
+#'
+#' This model was trained on a Russian media coprus (newspapers, TV transcripts
+#' and newswires) to analyze framing of street protests. The scale is protests
+#' as "freedom of expression" (high) vs "social disorder" (low). Although some
+#' slots are missing in this object (because the model was imported from the
+#' original Python implementation), it allows you to scale texts using
+#' \code{predict}.
+#' @name textmodel_lss_russianprotests
+#' @docType data
+#' @author Kohei Watanabe \email{watanabe.kohei@gmail.com}
+#' @keywords data
+#' @references “‘Russian Spring’ or ‘Spring Betrayal’? The Media as a Mirror of
+#'   Putin’s Evolving Strategy in Ukraine.” Taylor & Francis. Accessed March 9,
+#'   2018. https://www.tandfonline.com/doi/abs/10.1080/09668136.2017.1397603.
+NULL
