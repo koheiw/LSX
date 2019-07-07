c7d3f4ee94018efe199cc2152af52269d1084761
Kohei Watanabe <watanabe.kohei@gmail.com>
2019-07-07 18:28:05 +0200

    Skip utils on travis

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,8 @@
 test_that("diagnosys works", {
-  txt <- c("a b c d 123 . (xxx) \u2700", "a b_c 1st 2nd xxx@gmail.com",
+    skip_on_travis()
+    txt <- c("a b c d 123 . (xxx) \u2700", "a b_c 1st 2nd xxx@gmail.com",
            "Aa bb cc. Xx yy zz.", "Aa bb cc. Xx yy zz.")
-  dat <- data.frame(document = paste0("text", 1:4),
+    dat <- data.frame(document = paste0("text", 1:4),
                     number = c(1, 2, 0, 0),
                     punct = c(3, 2, 2, 2),
                     symbol = c(1, 0, 0, 0),
@@ -10,7 +11,7 @@ test_that("diagnosys works", {
                     dupli = c(FALSE, FALSE, FALSE, TRUE),
                     noise = c(0.5, 0.8, 0.25, 0.25),
                     stringsAsFactors = FALSE)
-  expect_equal(diagnosys(txt), dat)
+    expect_equal(diagnosys(txt), dat)
 
 })
 
