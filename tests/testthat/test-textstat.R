dc45d93868051350e39ae20807e5df9d1efb3141
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-11-20 11:14:24 +0900

    Fix tests

--- tests/testthat/test-textstat.R
+++ tests/testthat/test-textstat.R
@@ -17,18 +17,18 @@ test_that("char_context removes multi-word target", {
 
     key_rp <- textstat_context(toks_test, phrase("united states"),
                                min_count = 1, window = 0)
-    expect_identical(nrow(key_rp), 0)
+    expect_equal(nrow(key_rp), 0)
     suppressWarnings({
         feat_rp <- char_context(toks_test, phrase("united states"),
                                 min_count = 1, p = 0.05, window = 0)
     })
-    expect_identical(length(feat_rp), 0)
+    expect_equal(length(feat_rp), 0)
 
     key_kp <- textstat_context(toks_test, phrase("united states"),
                                min_count = 1, window = 0, remove_pattern = FALSE)
-    expect_identical(nrow(key_kp), 2)
+    expect_equal(nrow(key_kp), 2)
 
     feat_kp <- char_keyness(toks_test, phrase("united states"),
                             min_count = 1, p = 0.05, window = 0, remove_pattern = FALSE)
-    expect_identical(feat_kp == "united")
+    expect_identical(feat_kp, "united")
 })
