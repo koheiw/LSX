b6eec42a59c02c008450afd6cbc4e6fb37a9b1b2
Kohei Watanabe <watanabe.kohei@gmail.com>
2021-07-16 06:17:25 +0900

    Add tests

--- tests/testthat/test-textplot.R
+++ tests/testthat/test-textplot.R
@@ -1,13 +1,27 @@
 context("test textplot_*")
 
+require(quanteda)
 toks_test <- readRDS("../data/tokens_test.RDS")
 test_toks <- tokens_remove(toks_test, stopwords())
 dict <- dictionary(list("keywords" = c("positive", "bad", "xxxx")))
 
-test_that("textplot_* works", {
+test_that("textplot_* works with SVD", {
     dfmt <- dfm(test_toks)
     seed <- c("nice*" = 1, "positive*" = 1, "bad*" = -1, "negative*" = -1)
     lss <- textmodel_lss(dfmt, seed, k = 10)
+    expect_equal(class(textplot_simil(lss, group = TRUE)), c("gg", "ggplot"))
+    expect_equal(class(textplot_simil(lss, group = FALSE)), c("gg", "ggplot"))
+    expect_equal(class(textplot_terms(lss, highlighted = dict$keywords)),
+                 c("gg", "ggplot"))
+    expect_equal(class(textplot_terms(lss, highlighted = dict)),
+                 c("gg", "ggplot"))
+    expect_equal(class(textplot_terms(lss)), c("gg", "ggplot"))
+})
+
+test_that("textplot_* works with Glove", {
+    fcmt <- fcm(test_toks)
+    seed <- c("nice*" = 1, "positive*" = 1, "bad*" = -1, "negative*" = -1)
+    lss <- textmodel_lss(fcmt, seed, w = 10)
     expect_equal(class(textplot_simil(lss, group = TRUE)), c("gg", "ggplot"))
     expect_equal(class(textplot_simil(lss, group = FALSE)), c("gg", "ggplot"))
     expect_equal(class(textplot_terms(lss, highlighted = dict$keywords)),
