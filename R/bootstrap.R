702c404530f293b59cb7b0b2dce88c40ab09dcae
Kohei Watanabe <watanabe.kohei@gmail.com>
2022-02-09 08:10:41 +0900

    Add candidate()

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,4 +1,4 @@
-#' \[experimental\] Compute polarity scores of words with different hyper-parameters.
+#' \[experimental\] Compute polarity scores of words with different hyper-parameters
 #' @param x a fitted textmodel_lss object.
 #' @param what choose the parameter to resample in bootstrapping.
 #' @param by only for "k"
@@ -34,3 +34,20 @@ bootstrap_lss <- function(x, what = c("seeds", "k", "slice"),
     return(result)
 }
 
+#' \[experimental\] Find candidates for seed words through bootstrapping
+#' @param x a fitted textmodel_lss object.
+#' @param rank the lowest rank of candidates returned from [bootstrap_lss()].
+#' @keywords internal
+#' @export
+candidates <- function(x, rank = 10, ...) {
+    result <- list()
+    cat(sprintf("Searching words similar to %d seed words...\n", length(x$seeds_weighted)))
+    for (seed in names(x$seeds_weighted)) {
+        cat("  ", seed, "...\n")
+        bs_slice <- bootstrap_lss(as.textmodel_lss(x, seeds = seed), what = "slice", ...)
+        cand <- unique(as.character(head(bs_slice$terms, rank)))
+        bs_seed <- bootstrap_lss(as.textmodel_lss(x, seeds = cand), what = "seeds")
+        result[[seed]] <- names(sort(proxyC::colSds(bs_seed$beta), decreasing = TRUE))
+    }
+    return(result)
+}
