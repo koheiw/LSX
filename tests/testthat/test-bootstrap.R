589b04f579de11f0f5fb6445430f3f919ffa85c9
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-02-26 09:22:02 +0900

    Add arguments to bootstrap_lss()

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -16,7 +16,7 @@ test_that("bootstrap_lss works", {
     expect_equal(nrow(bs2), length(lss_test$beta))
     expect_equal(attr(bs2, "values"), seq(50, 300, 50))
 
-    bs3 <- bootstrap_lss(lss_test, output = "coef")
+    bs3 <- bootstrap_lss(lss_test, mode = "coef")
     expect_equal(class(as.vector(bs3)), "numeric")
     expect_equal(ncol(bs3), 12)
     expect_equal(nrow(bs3), length(lss_test$beta))
@@ -26,10 +26,29 @@ test_that("bootstrap_lss works", {
     expect_equal(ncol(bs4), 26)
     expect_equal(attr(bs4, "values"), seq(50, 300, 10))
 
+    expect_error(bootstrap_lss(lss_test, what = "k", from = 0),
+                 "The value of from must be between 1 and 300")
+    expect_error(bootstrap_lss(lss_test, what = "k", to = 0),
+                 "The value of to must be between 1 and 300")
     expect_error(bootstrap_lss(lss_test, what = "k", by = -1),
                  "The value of by must be between 1 and 250")
     expect_error(bootstrap_lss(lss_test, what = "k", by = 1000),
                  "The value of by must be between 1 and 250")
 
+})
+
+test_that("bootstrap_lss show messages", {
+
+    expect_silent(
+        bootstrap_lss(lss_test, "k", verbose = FALSE)
+    )
+    expect_output(
+        bootstrap_lss(lss_test, "seeds", verbose = TRUE),
+        "Fitting textmodel_lss with a different hyper-parameter.*"
+    )
+    expect_output(
+        bootstrap_lss(lss_test, "k", verbose = TRUE),
+        "Fitting textmodel_lss with a different hyper-parameter.*"
+    )
 })
 
