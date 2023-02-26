01d9425a599d76cf7ea57db0f148d0d92119db25
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-02-26 09:44:36 +0900

    Update MAN

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,14 +1,14 @@
-#' \[experimental\] Compute polarity scores different hyper-parameters
+#' \[experimental\] Compute polarity scores with different hyper-parameters
 #'
 #' A function to compute polarity scores of words by resampling hyper-parameters
 #' from a fitted LSS model.
 #' @param x a fitted textmodel_lss object.
 #' @param what choose the parameter to resample in bootstrapping.
-#' @param mode choose the types of the output object. If `coef`, returns the
-#'   polarity scores of words; if `terms`, returns words sorted by the polarity
-#'   scores in descending order.
-#' @param from,to,by passed to `seq()` to generate values for `k`; only used for when
-#'   `what = "k"`.
+#' @param mode choose the type of the result of bootstrapping. If `coef`,
+#'   returns polarity scores; if `terms`, returns words sorted by
+#'   the polarity scores in descending order.
+#' @param from,to,by passed to `seq()` to generate values for `k`; only used
+#'   when `what = "k"`.
 #' @param ... additional arguments passed to `as.textmodel_lss()`.
 #' @param verbose show messages if `TRUE`.
 #' @export
