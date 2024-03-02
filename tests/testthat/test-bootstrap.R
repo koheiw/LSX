e084aa9f20d7f9c7079c54643b80dc8ad83347c4
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-03-02 12:00:36 +0900

    Add remove argument to bootstrap_lss()

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -2,7 +2,8 @@ require(quanteda)
 
 lss_test <- readRDS("../data/lss_test.RDS")
 
-test_that("bootstrap_lss works", {
+test_that("bootstrap_lss works with what = seeds", {
+
     bs1 <- bootstrap_lss(lss_test, "seeds")
     expect_true(is.character(as.vector(bs1)))
     expect_equal(class(as.vector(bs1)), "character")
@@ -10,11 +11,12 @@ test_that("bootstrap_lss works", {
     expect_equal(nrow(bs1), length(lss_test$beta))
     expect_equal(attr(bs1, "values"), names(lss_test$seeds_weighted))
 
-    bs2 <- bootstrap_lss(lss_test, what = "k")
-    expect_equal(class(as.vector(bs2)), "character")
-    expect_equal(ncol(bs2), 6)
+    bs2 <- bootstrap_lss(lss_test, "seeds", remove = TRUE)
+    expect_true(is.character(as.vector(bs2)))
+    expect_equal(class(as.vector(bs1)), "character")
+    expect_equal(ncol(bs2), 12)
     expect_equal(nrow(bs2), length(lss_test$beta))
-    expect_equal(attr(bs2, "values"), seq(50, 300, 50))
+    expect_equal(attr(bs2, "values"), names(lss_test$seeds_weighted))
 
     bs3 <- bootstrap_lss(lss_test, mode = "coef")
     expect_equal(class(as.vector(bs3)), "numeric")
@@ -22,13 +24,35 @@ test_that("bootstrap_lss works", {
     expect_equal(nrow(bs3), length(lss_test$beta))
     expect_equal(attr(bs3, "values"), names(lss_test$seeds_weighted))
 
-    bs4 <- bootstrap_lss(lss_test, what = "k", by = 10)
-    expect_equal(ncol(bs4), 26)
-    expect_equal(attr(bs4, "values"), seq(50, 300, 10))
+})
 
-    bs5 <- bootstrap_lss(lss_test, what = "k", from = 100, to = 200, by = 10)
-    expect_equal(ncol(bs5), 11)
-    expect_equal(attr(bs5, "values"), seq(100, 200, 10))
+test_that("bootstrap_lss works with remove = TRUE", {
+    lss_test$terms <- NULL
+
+    bs4 <- bootstrap_lss(lss_test, "seeds")
+    expect_true(identical(colnames(bs4), unname(bs4[1,])))
+    bs5 <- bootstrap_lss(lss_test, "seeds", remove = TRUE)
+    expect_false(identical(colnames(bs5), unname(bs5[1,])))
+
+    expect_error(bootstrap_lss(lss_test, what = "seed", remove = NULL),
+                 "remove cannot be NULL")
+})
+
+test_that("bootstrap_lss with what = k", {
+
+    bs1 <- bootstrap_lss(lss_test, what = "k")
+    expect_equal(class(as.vector(bs1)), "character")
+    expect_equal(ncol(bs1), 6)
+    expect_equal(nrow(bs1), length(lss_test$beta))
+    expect_equal(attr(bs1, "values"), seq(50, 300, 50))
+
+    bs2 <- bootstrap_lss(lss_test, what = "k", by = 10)
+    expect_equal(ncol(bs2), 26)
+    expect_equal(attr(bs2, "values"), seq(50, 300, 10))
+
+    bs3 <- bootstrap_lss(lss_test, what = "k", from = 100, to = 200, by = 10)
+    expect_equal(ncol(bs3), 11)
+    expect_equal(attr(bs3, "values"), seq(100, 200, 10))
 
     expect_error(bootstrap_lss(lss_test, what = "k", from = 0),
                  "The value of from must be between 1 and 300")
@@ -38,7 +62,6 @@ test_that("bootstrap_lss works", {
                  "The value of by must be between 1 and 250")
     expect_error(bootstrap_lss(lss_test, what = "k", by = 1000),
                  "The value of by must be between 1 and 250")
-
 })
 
 test_that("bootstrap_lss show messages", {
