14cbbdd8e274114c77de049785243a2f8f394d90
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-04-04 20:38:48 +0900

    Update replication test

--- tests/misc/replication.R
+++ tests/misc/replication.R
@@ -1,8 +1,10 @@
 require(quanteda)
 require(wordvector)
 require(LSX)
+require(testthat)
 
-as.textmodel_lss2 <- function(x, seeds) {
+# original function
+as.textmodel_lss0 <- function(x, seeds) {
 
   prob <- probability(x, names(seeds), "values")
   seeds <- seeds[names(seeds) %in% rownames(prob)]
@@ -21,9 +23,16 @@ seed <- as.seedwords(data_dictionary_sentiment)
 wdv <- textmodel_word2vec(toks, dim = 100, type = "skip-gram", normalize = FALSE,
                           verbpse = TRUE)
 
-lss0 <- as.textmodel_lss2(wdv, seed)
+lss0 <- as.textmodel_lss0(wdv, seed)
 lss <- as.textmodel_lss(wdv, seed, spatial = FALSE)
 
-setequal(names(lss$beta), names(lss0$beta))
-cor(coef(lss0), coef(lss))
+expect_setequal(
+  names(lss$beta),
+  names(lss0$beta)
+)
+
+expect_equal(
+  cor(coef(lss0), coef(lss)),
+  1.0
+)
 
