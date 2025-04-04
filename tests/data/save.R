672ebf03be1d66ab8e9b5f4ca8e53ab10117c5a8
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-04-04 14:20:41 +0900

    Create new test objects

--- tests/data/save.R
+++ tests/data/save.R
@@ -1,13 +1,17 @@
 require(quanteda)
 require(wordvector)
 
-toks_test <- readRDS("../data/tokens_test.RDS")
+toks_test <- readRDS("tests/data/tokens_test.RDS") %>%
+  tokens_tolower()
 feat_test <- head(char_context(toks_test, "america*", min_count = 1, p = 0.05), 100)
 dfmt_test <- dfm(toks_test)
 seed_test <- as.seedwords(data_dictionary_sentiment)
 
 lss_test <- textmodel_lss(dfmt_test, seed_test, terms = feat_test, k = 300)
-saveRDS(lss_test, "../data/lss_test.RDS")
+saveRDS(lss_test, "tests/data/lss_test.RDS")
 
-w2v_test <- word2vec(head(toks, 10), min_count = 1)
-saveRDS(w2v_test, "tests/data/word2vec_test.RDS")
+wdv_test <- textmodel_word2vec(head(toks_test, 10), min_count = 1)
+saveRDS(wdv_test, "tests/data/word2vec_test.RDS")
+
+wdv_test2 <- textmodel_word2vec(head(toks_test, 10), min_count = 1, normalize = FALSE)
+saveRDS(wdv_test2, "tests/data/word2vec-prob_test.RDS")
