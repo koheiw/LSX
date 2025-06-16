25750093476fca1d3a8867f6fe907c1899b94257
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-06-17 08:36:52 +0900

    Add textmodel_pss()

---
+++ R/textmodel_pss.R
@@ -0,0 +1,53 @@
+#' \[experimental\] Fit a Probabilistic Semantic Scaling model
+#'
+#' Probabilistic Semantic Scaling (PSS) is an algorithm for
+#' document scaling based on language models.
+#' @param x a [quanteda::tokens] or [quanteda::tokens_xptr] object.
+#' @param engine currently only support the word2vec model with the skip-gram algorithm.
+#' @export
+#' @inheritParams textmodel_lss
+textmodel_pss <- function(x, seeds, terms = NULL, k = 200,
+                          engine = c("word2vec"),
+                          tolower = TRUE,
+                          include_data = FALSE,
+                          group_data = FALSE,
+                          verbose = FALSE, ...) {
+  UseMethod("textmodel_pss")
+}
+
+
+#' @export
+textmodel_pss.tokens <- function(x, seeds, terms = NULL, k = 200,
+                                 engine = c("word2vec"),
+                                 tolower = TRUE,
+                                 include_data = FALSE,
+                                 group_data = FALSE,
+                                 verbose = FALSE, ...) {
+
+
+  k <- check_integer(k, min = 2)
+  engine <- match.arg(engine)
+
+  if (!requireNamespace("wordvector"))
+    stop("wordvector package must be installed")
+  if (utils::packageVersion("wordvector") < as.numeric_version("0.5.0"))
+    stop("wordvector package must be v0.5.0 or later")
+
+  w2v <- wordvector::textmodel_word2vec(x, dim = k, type = "skip-gram", tolower = tolower,
+                                        normalize = FALSE, verbose = verbose, ...)
+  result <- as.textmodel_lss(w2v, seeds = seeds, terms = terms, verbose = verbose)
+  result$call <- try(match.call(sys.function(-1), call = sys.call(-1)), silent = TRUE)
+
+  if (include_data) {
+    x <- dfm(x, remove_padding = TRUE, tolower = tolower)
+    if (group_data) {
+      result$data <- dfm_group(x)
+    } else {
+      result$data <- x
+    }
+  } else {
+    if (group_data)
+      warning("group_data is ignored when include_data = FALSE", call. = FALSE)
+  }
+  return(result)
+}
---
+++ tests/testthat/test-textmodel_pss.R
@@ -0,0 +1,58 @@
+require(quanteda)
+
+# create and save test object
+# corp_sent <- corpus_reshape(data_corpus_inaugural, "sentence")
+# toks_test <- tokens(corp_sent, remove_punct = TRUE)
+# saveRDS(toks_test, "tests/data/tokens.RDS")
+
+toks_test <- readRDS("../data/tokens.RDS")
+feat_test <- head(char_context(toks_test, "america*", min_count = 1, p = 0.05), 100)
+seed <- as.seedwords(data_dictionary_sentiment)
+
+test_that("textmodel_pss works", {
+
+  # without data
+  pss1 <- textmodel_pss(toks_test, seed, k = 10)
+
+  expect_equal(pss1$k, 10)
+  expect_equal(names(pss1$beta),
+               types(tokens_trim(tokens_tolower(toks_test), min_termfreq = 5)))
+  expect_equal(pss1$concatenator, concatenator(toks_test))
+  expect_equal(pss1$slice, NULL)
+  expect_equal(pss1$embedding, NULL)
+  expect_equal(pss1$data, NULL)
+  expect_error(
+    predict(pss1),
+    "The model includes no data, use newdata to supply a dfm."
+  )
+
+  # with data
+  pss2 <- textmodel_pss(toks_test, seed, k = 10, include_data = TRUE)
+
+  expect_equal(pss2$concatenator, concatenator(toks_test))
+  expect_equal(docnames(pss2$data), docnames(toks_test))
+  expect_equal(
+    names(predict(pss2)),
+    docnames(toks_test)
+  )
+
+  # with terms
+  pss3 <- textmodel_pss(toks_test, seed, k = 10, terms = feat_test,
+                        include_data = TRUE, group_data = TRUE)
+
+  expect_true(all(names(pss3$beta) %in% feat_test))
+  expect_equal(
+    names(predict(pss3)),
+    docnames(tokens_group(toks_test))
+  )
+
+  # error
+  expect_error(
+    textmodel_pss(toks_test, k = -1),
+    "The value of k must be between 2 and Inf"
+  )
+  expect_error(
+    textmodel_pss(toks_test, k = c(10, 20)),
+    "The length of k must be 1"
+  )
+})
