8fefa2427c26386c1391886568fac5d416ffde5a
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-06-17 08:56:59 +0900

    Add more tests

--- tests/testthat/test-textmodel_pss.R
+++ tests/testthat/test-textmodel_pss.R
@@ -11,9 +11,12 @@ seed <- as.seedwords(data_dictionary_sentiment)
 
 test_that("textmodel_pss works", {
 
+  skip_on_cran()
+
   # without data
   pss1 <- textmodel_pss(toks_test, seed, k = 10)
 
+  expect_s3_class(pss1, "textmodel_lss")
   expect_equal(pss1$k, 10)
   expect_equal(names(pss1$beta),
                types(tokens_trim(tokens_tolower(toks_test), min_termfreq = 5)))
@@ -29,6 +32,7 @@ test_that("textmodel_pss works", {
   # with data
   pss2 <- textmodel_pss(toks_test, seed, k = 10, include_data = TRUE)
 
+  expect_s3_class(pss2, "textmodel_lss")
   expect_equal(pss2$concatenator, concatenator(toks_test))
   expect_equal(docnames(pss2$data), docnames(toks_test))
   expect_equal(
@@ -40,11 +44,19 @@ test_that("textmodel_pss works", {
   pss3 <- textmodel_pss(toks_test, seed, k = 10, terms = feat_test,
                         include_data = TRUE, group_data = TRUE)
 
+  expect_s3_class(pss3, "textmodel_lss")
   expect_true(all(names(pss3$beta) %in% feat_test))
   expect_equal(
     names(predict(pss3)),
     docnames(tokens_group(toks_test))
   )
+
+  # with tokens_xptr
+  pss4 <- textmodel_pss(as.tokens_xptr(toks_test), seed, k = 10,
+                        include_data = TRUE)
+
+  expect_s3_class(pss4, "textmodel_lss")
+  expect_equal(docnames(pss4$data), docnames(toks_test))
 
   # error
   expect_error(
@@ -55,4 +67,5 @@ test_that("textmodel_pss works", {
     textmodel_pss(toks_test, k = c(10, 20)),
     "The length of k must be 1"
   )
+
 })
