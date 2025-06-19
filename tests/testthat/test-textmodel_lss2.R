1081bac923af0d9e4d87e48f073b910a0ce00664
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-06-20 07:54:14 +0900

    Add tests for spatial = TRUE and FALSE

--- tests/testthat/test-textmodel_lss2.R
+++ tests/testthat/test-textmodel_lss2.R
@@ -9,54 +9,54 @@ toks_test <- readRDS("../data/tokens.RDS")
 feat_test <- head(char_context(toks_test, "america*", min_count = 1, p = 0.05), 100)
 seed <- as.seedwords(data_dictionary_sentiment)
 
-test_that("textmodel_lss works", {
+test_that("textmodel_lss works when spatial = TRUE", {
 
   skip_on_cran()
 
   # without data
-  pss1 <- textmodel_lss(toks_test, seed, k = 10)
+  lss1 <- textmodel_lss(toks_test, seed, k = 10)
 
-  expect_s3_class(pss1, "textmodel_lss")
-  expect_equal(pss1$k, 10)
-  expect_equal(names(pss1$beta),
+  expect_s3_class(lss1, "textmodel_lss")
+  expect_equal(lss1$k, 10)
+  expect_equal(names(lss1$beta),
                types(tokens_trim(tokens_tolower(toks_test), min_termfreq = 5)))
-  expect_equal(pss1$concatenator, concatenator(toks_test))
-  expect_equal(pss1$slice, NULL)
-  expect_equal(pss1$embedding, NULL)
-  expect_equal(pss1$data, NULL)
+  expect_equal(lss1$concatenator, concatenator(toks_test))
+  expect_equal(lss1$slice, 1:10)
+  expect_equal(dim(lss1$embedding), c(10, length(lss1$beta)))
+  expect_equal(lss1$data, NULL)
   expect_error(
-    predict(pss1),
+    predict(lss1),
     "The model includes no data, use newdata to supply a dfm."
   )
 
   # with data
-  pss2 <- textmodel_lss(toks_test, seed, k = 10, include_data = TRUE)
+  lss2 <- textmodel_lss(toks_test, seed, k = 10, include_data = TRUE)
 
-  expect_s3_class(pss2, "textmodel_lss")
-  expect_equal(pss2$concatenator, concatenator(toks_test))
-  expect_equal(docnames(pss2$data), docnames(toks_test))
+  expect_s3_class(lss2, "textmodel_lss")
+  expect_equal(lss2$concatenator, concatenator(toks_test))
+  expect_equal(docnames(lss2$data), docnames(toks_test))
   expect_equal(
-    names(predict(pss2)),
+    names(predict(lss2)),
     docnames(toks_test)
   )
 
   # with terms
-  pss3 <- textmodel_lss(toks_test, seed, k = 10, terms = feat_test,
+  lss3 <- textmodel_lss(toks_test, seed, k = 10, terms = feat_test,
                          include_data = TRUE, group_data = TRUE)
 
-  expect_s3_class(pss3, "textmodel_lss")
-  expect_true(all(names(pss3$beta) %in% feat_test))
+  expect_s3_class(lss3, "textmodel_lss")
+  expect_true(all(names(lss3$beta) %in% feat_test))
   expect_equal(
-    names(predict(pss3)),
+    names(predict(lss3)),
     docnames(tokens_group(toks_test))
   )
 
   # with tokens_xptr
-  pss4 <- textmodel_lss(as.tokens_xptr(toks_test), seed, k = 10,
+  lss4 <- textmodel_lss(as.tokens_xptr(toks_test), seed, k = 10,
                          include_data = TRUE)
 
-  expect_s3_class(pss4, "textmodel_lss")
-  expect_equal(docnames(pss4$data), docnames(toks_test))
+  expect_s3_class(lss4, "textmodel_lss")
+  expect_equal(docnames(lss4$data), docnames(toks_test))
 
   # warning
   expect_warning(
@@ -76,3 +76,73 @@ test_that("textmodel_lss works", {
   )
 
 })
+
+
+test_that("textmodel_lss works when spatial = FALSE", {
+
+  skip_on_cran()
+
+  # without data
+  lss1 <- textmodel_lss(toks_test, seed, k = 10, spatial = FALSE)
+
+  expect_s3_class(lss1, "textmodel_lss")
+  expect_equal(lss1$k, 10)
+  expect_equal(names(lss1$beta),
+               types(tokens_trim(tokens_tolower(toks_test), min_termfreq = 5)))
+  expect_equal(lss1$concatenator, concatenator(toks_test))
+  expect_equal(lss1$slice, NULL)
+  expect_equal(lss1$embedding, NULL)
+  expect_equal(lss1$data, NULL)
+  expect_error(
+    predict(lss1),
+    "The model includes no data, use newdata to supply a dfm."
+  )
+
+  # with data
+  lss2 <- textmodel_lss(toks_test, seed, k = 10, include_data = TRUE, spatial = FALSE)
+
+  expect_s3_class(lss2, "textmodel_lss")
+  expect_equal(lss2$concatenator, concatenator(toks_test))
+  expect_equal(docnames(lss2$data), docnames(toks_test))
+  expect_equal(
+    names(predict(lss2)),
+    docnames(toks_test)
+  )
+
+  # with terms
+  lss3 <- textmodel_lss(toks_test, seed, k = 10, terms = feat_test,
+                        include_data = TRUE, group_data = TRUE, spatial = FALSE)
+
+  expect_s3_class(lss3, "textmodel_lss")
+  expect_true(all(names(lss3$beta) %in% feat_test))
+  expect_equal(
+    names(predict(lss3)),
+    docnames(tokens_group(toks_test))
+  )
+
+  # with tokens_xptr
+  lss4 <- textmodel_lss(as.tokens_xptr(toks_test), seed, k = 10,
+                        include_data = TRUE, spatial = FALSE)
+
+  expect_s3_class(lss4, "textmodel_lss")
+  expect_equal(docnames(lss4$data), docnames(toks_test))
+
+  # warning
+  expect_warning(
+    textmodel_lss(toks_test, seed, k = 10,
+                  include_data = FALSE, group_data = TRUE, spatial = FALSE),
+    "group_data is ignored when include_data = FALSE"
+  )
+
+  # error
+  expect_error(
+    textmodel_lss(toks_test, k = -1, spatial = FALSE),
+    "The value of k must be between 2 and Inf"
+  )
+  expect_error(
+    textmodel_lss(toks_test, k = c(10, 20), spatial = FALSE),
+    "The length of k must be 1"
+  )
+
+})
+
