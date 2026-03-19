597d31a94a28a083c175bbdfeb3abcfe6fe0e694
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-03-19 10:33:35 +0900

    Save doc2vec for tests

--- tests/data/save.R
+++ tests/data/save.R
@@ -12,8 +12,12 @@ seed <- as.seedwords(data_dictionary_sentiment)
 lss <- textmodel_lss(dfmt, seed, terms = feat, k = 300)
 saveRDS(lss, "tests/data/lss_k300.RDS")
 
-wdv <- textmodel_word2vec(head(toks, 10), min_count = 1)
-saveRDS(wdv, "tests/data/word2vec.RDS")
+wov <- textmodel_word2vec(head(toks, 10), min_count = 1)
+saveRDS(wov, "tests/data/word2vec.RDS")
 
-wdv2 <- textmodel_word2vec(head(toks, 10), min_count = 1, normalize = FALSE)
-saveRDS(wdv2, "tests/data/word2vec-prob.RDS")
+wov2 <- textmodel_word2vec(head(toks, 10), min_count = 1, normalize = FALSE)
+saveRDS(wov2, "tests/data/word2vec-prob.RDS")
+
+dov <- textmodel_doc2vec(head(toks, 100), type = "dbow", min_count = 1,
+                         include_data = TRUE)
+saveRDS(dov, "tests/data/doc2vec.RDS")
