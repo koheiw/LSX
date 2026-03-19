240ac15b11ef928f43b54cd1bdee7ee00ed17137
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-03-19 10:32:34 +0900

    Separate lss, lss2, lss3

--- R/as.textmodel2.R
+++ R/as.textmodel2.R
@@ -1,27 +1,63 @@
-rowMaxs <- function(x) {
-  structure(x[cbind(seq_len(nrow(x))), max.col(x, "first")],
-            names = rownames(x))
-}
 
-as.textmodel_lss2 <- function(x, seeds) {
+#' @rdname as.textmodel_lss
+#' @export
+#' @method as.textmodel_lss textmodel_word2vec
+as.textmodel_lss.textmodel_word2vec <- function(x, seeds,
+                                                  terms = NULL,
+                                                  nested_weight = TRUE,
+                                                  verbose = FALSE,
+                                                  spatial = TRUE,
+                                                  ...) {
 
-  x$seeds <- seed
-  x$seeds_weighted <- expand_seeds(seeds, names(x$frequency))
-  class(x) <- c("textmodel_lss2", class(x))
-  return(x)
+  #args <- list(terms = terms, seeds = seeds)
+  spatial <- check_logical(spatial)
+  if (spatial) {
 
-}
+    if (x$version == as.numeric_version("0.1.0")) {
+      values <- x$vector
+    } else if (x$version >= as.numeric_version("0.6.0")) {
+      values <- x$values$word
+    } else {
+      values <- x$values
+    }
+    result <- as.textmodel_lss(t(values), seeds = seeds, terms = terms,
+                               nested_weight = nested_weight, ...)
+    result$frequency <- x$frequency[names(result$beta)]
+    result$type = "word2vec"
+    result$call = try(match.call(sys.function(-1), call = sys.call(-1)), silent = TRUE)
 
-predict.textmodel_lss2() <- function(x, min_n = 0L, mode = "mean") {
-  s <- unlist(unname(x$seed_weights))
-  p <- wordvector::probability(x, names(s), layer = "document", mode = "numeric")
-
-  if (mode == "mean") {
-    p <- rowMeans(p)
   } else {
-    p <- rowMaxs(p)
+
+    if (!requireNamespace("wordvector"))
+      stop("wordvector package must be installed")
+    if (x$version < as.numeric_version("0.2.0"))
+      stop("wordvector package must be v0.2.0 or later")
+
+    seeds <- expand_seeds(seeds, names(x$frequency), nested_weight, verbose)
+    seed <- unlist(unname(seeds))
+    theta <- get_theta(terms, names(x$frequency))
+
+    suppressWarnings({
+      if (packageVersion("wordvector") >= "0.6.0") {
+        prob <- wordvector::probability(x, names(seed), mode = "numeric")
+      } else {
+        prob <- wordvector::probability(x, names(seed), mode = "values")
+      }
+    })
+    beta <- rowSums(prob[names(theta),,drop = FALSE] %*% seed) * theta
+
+    result <- build_lss(
+      beta = beta,
+      beta_type = "probability",
+      k = x$dim,
+      terms = terms,
+      seeds = seeds,
+      seeds_weighted = seed,
+      frequency = x$frequency[names(beta)],
+      type = "word2vec",
+      spatial = FALSE,
+      call = try(match.call(sys.function(-1), call = sys.call(-1)), silent = TRUE)
+    )
   }
-  if (min_n > 0)
-    p <- p * (x$length / p(x$length, min_n))
-  return(p)
+  return(result)
 }
---
+++ R/as.textmodel3.R
@@ -0,0 +1,53 @@
+rowMaxs <- function(x) {
+  structure(x[cbind(seq_len(nrow(x))), max.col(x, "first")],
+            names = rownames(x))
+}
+
+#' @export
+#' @keywords internal
+#' @method as.textmodel_lss textmodel_doc2vec
+as.textmodel_lss.textmodel_doc2vec <- function(x, seeds, max_prob = FALSE) {
+
+  max_prob <- check_logical(max_prob)
+
+  seeds_weighted <- expand_seeds(seeds, names(x$frequency))
+  seed <- unlist(unname(seeds_weighted))
+  prob <- wordvector::probability(x, seed, layer = "document", mode = "numeric")
+
+  if (max_prob) {
+    alpha <- rowMaxs(prob)
+  } else {
+    alpha <- rowMeans(prob)
+  }
+
+  # pseudo beta
+  data <- dfm(x$data, remove_padding = TRUE)
+  data <- dfm_select(data, names(x$frequency), valuetype = "fixed",
+                     case_insensitive = TRUE)
+  freq <- colSums(data)
+  beta <- colSums(data * alpha) / freq
+
+  result <- build_lss(
+    beta = beta,
+    beta_type = "dummy",
+    terms = names(beta),
+    frequency = freq,
+    call = try(match.call(sys.function(-1), call = sys.call(-1)), silent = TRUE)
+  )
+  result$alpha <- alpha
+  result$length <- rowSums(data)
+
+  class(result) <- c("textmodel_lss3", class(result))
+  return(result)
+
+}
+
+#' @export
+#' @keywords internal
+#' @method predict textmodel_lss3
+predict.textmodel_lss3 <- function(x, min_n = 0L) {
+  p <- x$alphalss
+  if (min_n > 0)
+    p <- p * (x$length / pmax(x$length, min_n))
+  return(p)
+}
