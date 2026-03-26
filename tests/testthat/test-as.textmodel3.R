e230213ee6ad9ef2d6f61751596ed38e6f1066fe
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-03-26 17:18:57 +0900

    Add tests

---
+++ tests/testthat/test-as.textmodel3.R
@@ -0,0 +1,65 @@
+
+dov_test <- readRDS("../data/doc2vec.RDS")
+
+test_that("as.textmodel_lss is working", {
+
+  # single seed
+  lss1 <- as.textmodel_lss(dov_test, "good", max_prob = FALSE)
+  expect_false(
+    lss1$max_prob
+  )
+  expect_true(
+    all(predict(lss1) == lss1$alpha)
+  )
+  expect_false(
+    all(predict(lss1, min_n = 10) == lss1$alpha)
+  )
+  expect_true(
+    ggplot2::is_ggplot(textplot_terms(lss1))
+  )
+
+  lss2 <- as.textmodel_lss(dov_test, "good", max_prob = TRUE)
+  expect_true(
+    lss2$max_prob
+  )
+  expect_true(
+    all(predict(lss2) == lss2$alpha)
+  )
+  expect_false(
+    all(predict(lss2, min_n = 10) == lss2$alpha)
+  )
+  expect_equal(lss2$seeds, "good")
+  expect_equal(lss2$seeds_weighted,
+               list("good" = c("good" = 1)))
+  expect_identical(
+    predict(lss1),
+    predict(lss2)
+  )
+
+  # fixed seeds
+  seed <- c("good" = 1, "superior" = 1, "wrong" = -1, "nasty" = -1)
+  lss3 <- as.textmodel_lss(dov_test, seed, max_prob = FALSE)
+  lss4 <- as.textmodel_lss(dov_test, seed, max_prob = TRUE)
+  expect_gt(
+    cor(predict(lss3), predict(lss4)),
+    0.99
+  )
+
+  # glob seeds
+  seed <- c("america" = 1, "nation*" = 1, "foreign*" = -1)
+  lss5 <- as.textmodel_lss(dov_test, seed, max_prob = FALSE)
+  expect_equal(
+    lss5$seeds,
+    c("america" = 1, "nation*" = 1, "foreign*" = -1)
+  )
+  expect_equal(
+    lss5$seeds_weighted,
+    list("america" = c("america" = 0.25),
+         "nation*" = c("nations" = 0.25, "nation" = 0.25,  "national" = 0.25),
+         "foreign*" = c("foreign" = -1))
+  )
+  expect_true(
+    ggplot2::is_ggplot(textplot_terms(lss5))
+  )
+
+})
